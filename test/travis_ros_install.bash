d86acc42bf3993d035aae07c8aea9908526458c2
Shogo Kamikawaji <kamikologic@gmail.com>
2020-03-22 03:17:43 +0000

    try xenial

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,14 +1,13 @@
 #!/bin/bash -xve
 
 #required packages
-pip install --upgrade pip
 pip install catkin_pkg
 pip install empy
 pip install pyyaml
 pip install rospkg
 
 #ros install
-git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu16.04_server.git
 cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
